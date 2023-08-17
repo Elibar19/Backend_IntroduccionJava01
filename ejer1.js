bcaf6b8bc6a15e6b9f0cd7ab23f80da3387ce61f
Elián Bargaz <122175106+Elibar19@users.noreply.github.com>
2023-08-17 17:38:10 -0300

    Delete ejer1.js

--- ejer1.js
+++
@@ -1,7 +0,0 @@
-/* Escribir un algoritmo en el cual se consulte al usuario que ingrese ¿cómo está el día de 
-hoy? (soleado, nublado, lloviendo). A continuación, mostrar por pantalla un mensaje que 
-indique “El día de hoy está ...”, completando el mensaje con el dato que ingresó el usuario.  */
-
-let clima = prompt("¿Como está el día hoy?");
-
-alert("El día de hoy está "+ clima);
