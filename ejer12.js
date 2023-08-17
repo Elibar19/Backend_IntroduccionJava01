12a711e2cfa674d22ddac057db1a6178f805bf2d
Elián Bargaz <122175106+Elibar19@users.noreply.github.com>
2023-08-17 17:38:42 -0300

    Delete ejer12.js

--- ejer12.js
+++
@@ -1,7 +0,0 @@
-/* Escribir una función flecha de JavaScript que reciba un argumento y retorne el tipo de 
-dato */
-var dato = 42
-
-const resultado = dato => typeof dato
-
-alert(resultado(dato));
